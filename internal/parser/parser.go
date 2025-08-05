755cfadb87ade022635db514ded57f4b4aa64ce5
Sam-Brook <brooksyhap@gmail.com>
2025-08-05 19:21:09 +0800

    refactored multiple functions to handle different block level tags

--- internal/parser/parser.go
+++ internal/parser/parser.go
@@ -34,20 +34,28 @@ type Parser struct {
 
 func (s *SyntaxStack) Push(val string) {
 	s.topIndex++
-	if len(s.syntax) > s.topIndex+1 {
-		s.syntax = append(s.syntax, val)
-	} else {
+	if len(s.syntax) > s.topIndex {
 		s.syntax[s.topIndex] = val
+	} else {
+		s.syntax = append(s.syntax, val)
 	}
 }
 
 func (s *SyntaxStack) Pop() string {
+	if s.topIndex == -1 {
+		fmt.Println("attempted to pop from empty stack")
+		return ""
+	}
 	popped_elem := s.syntax[s.topIndex]
 	s.topIndex--
 	return popped_elem
 }
 
 func (s *SyntaxStack) Peek() string {
+	if s.topIndex == -1 {
+		fmt.Println("attempted to peek from empty stack")
+		return ""
+	}
 	popped_elem := s.syntax[s.topIndex]
 	return popped_elem
 }
@@ -89,15 +97,15 @@ func NewParser(input *string) *Parser {
 		LineIndex: 0,
 		CharIndex: 0,
 		BlockLevelTags: SyntaxStack{
-			syntax:   make([]string, 1),
+			syntax:   make([]string, 0),
 			topIndex: -1,
 		},
 		MultiLineTags: SyntaxStack{
-			syntax:   make([]string, 1),
+			syntax:   make([]string, 0),
 			topIndex: -1,
 		},
 		InTextTags: SyntaxStack{
-			syntax:   make([]string, 10),
+			syntax:   make([]string, 0),
 			topIndex: -1,
 		},
 	}
@@ -108,16 +116,25 @@ func GetPrefixToken(input string) string {
 	return input[0 : first_space_index+1]
 }
 
-func WrapLineWithTag(p *Parser, line string) {
-	element := blockhtmlTags[p.BlockLevelTags.Pop()]
-	p.Output.WriteString(element.Open)
-	p.Output.WriteString(line)
-	p.Output.WriteString(element.Close)
-	p.Output.WriteString("\n")
+func isTagChar(char byte) bool {
+	switch char {
+	case '*', '~', '`':
+		return true
+	default:
+		return false
+	}
 }
 
-func stripBlockTag(current_line string) string {
-	return current_line[strings.IndexAny(current_line, " ")+1:]
+func WriteTagPrefix(stack *SyntaxStack, md_tag string, output *strings.Builder) {
+	stack.Push(md_tag)
+	html_tag := stack.GetElement()
+	output.WriteString(html_tag.Open)
+}
+
+func WriteTagSuffix(stack *SyntaxStack, output *strings.Builder) {
+	html_tag := stack.GetElement()
+	stack.Pop()
+	output.WriteString(html_tag.Close)
 }
 
 func Parse(input string) string {
@@ -125,15 +142,57 @@ func Parse(input string) string {
 
 	fmt.Println("test input value: ", input)
 
+	// Line level loop
 	for ; p.LineIndex < len(p.Lines); p.LineIndex++ {
 		current_line := p.Lines[p.LineIndex]
+
 		first_word := GetPrefixToken(current_line)
-		_, ok := blockhtmlTags[first_word]
-		if ok {
-			p.BlockLevelTags.Push(first_word)
-			fmt.Println("test stack peek", p.BlockLevelTags.Peek())
+		fmt.Println("first word is : " + first_word)
+		_, block_tag_ok := blockhtmlTags[first_word]
+		if block_tag_ok {
+			WriteTagPrefix(&p.BlockLevelTags, first_word, &p.Output)
+
+			fmt.Println("block level tag is: " + p.BlockLevelTags.Peek())
+			fmt.Println("top index is : ", p.BlockLevelTags.topIndex)
+			fmt.Println("syntax stack is : ", p.BlockLevelTags.syntax)
+			p.CharIndex = p.CharIndex + len(first_word)
 		}
-		WrapLineWithTag(p, stripBlockTag(current_line))
+
+		// Char level loop
+		for ; p.CharIndex < len(current_line); p.CharIndex++ {
+			c := current_line[p.CharIndex]
+			if isTagChar(c) {
+
+				if ((p.CharIndex + 1) < len(current_line)) && (c == current_line[p.CharIndex+1]) {
+					tag := string(c + current_line[p.CharIndex+1])
+					fmt.Println("current tag is: " + tag)
+
+					if p.InTextTags.Peek() == tag {
+						WriteTagSuffix(&p.InTextTags, &p.Output)
+					} else {
+						WriteTagPrefix(&p.InTextTags, tag, &p.Output)
+					}
+					p.CharIndex = p.CharIndex + 2
+					continue
+				} else {
+					if p.InTextTags.Peek() == string(c) {
+						WriteTagSuffix(&p.InTextTags, &p.Output)
+					} else {
+						WriteTagPrefix(&p.InTextTags, string(c), &p.Output)
+					}
+					p.CharIndex = p.CharIndex + 1
+				}
+			} else {
+				p.Output.WriteByte(c)
+				continue
+			}
+		}
+		if block_tag_ok {
+			WriteTagSuffix(&p.BlockLevelTags, &p.Output)
+		}
+
+		p.Output.WriteByte('\n')
+		p.CharIndex = 0
 
 	}
 
